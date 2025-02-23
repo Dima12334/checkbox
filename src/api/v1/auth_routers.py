45162b1d09046fc88a125bf5019460264d705788
Dmytro Podolynnyi <podolinnyjdima@gmail.com>
2025-02-23 22:25:53 +0200

    refactoring

--- src/api/v1/auth_routers.py
+++ src/api/v1/auth_routers.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
 from config.database import get_async_session
 from src.core.schemas.auth_schemas import SignUpSchema, SignInSchema, JWTTokenSchema
@@ -9,13 +10,17 @@ from src.core.services.auth_service import AuthService
 auth_router = APIRouter()
 
 
-@auth_router.post("/sign-up", response_model=UserReadSchema)
+@auth_router.post(
+    "/sign-up", response_model=UserReadSchema, status_code=status.HTTP_201_CREATED
+)
 async def sign_up(schema: SignUpSchema, db: AsyncSession = Depends(get_async_session)):
     auth_servie = AuthService()
     return await auth_servie.sign_up(user_info=schema, db=db)
 
 
-@auth_router.post("/sign-in", response_model=JWTTokenSchema)
+@auth_router.post(
+    "/sign-in", response_model=JWTTokenSchema, status_code=status.HTTP_200_OK
+)
 async def sign_in(schema: SignInSchema, db: AsyncSession = Depends(get_async_session)):
     auth_servie = AuthService()
     return await auth_servie.sign_in(user_info=schema, db=db)
