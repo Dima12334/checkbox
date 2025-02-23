f3313449302aa26177d8dfbab0886711a53cd72c
Dmytro Podolynnyi <podolinnyjdima@gmail.com>
2025-02-23 10:44:39 +0200

    refactoring

--- src/core/filters/receipt_filters.py
+++ src/core/filters/receipt_filters.py
@@ -3,19 +3,25 @@ from decimal import Decimal
 from typing import Optional
 
 from fastapi_filter.contrib.sqlalchemy import Filter
+from pydantic import Field
 
 from src.receipts.constants import ReceiptConstants
 from src.receipts.models import Receipt
 
 
 class ReceiptFilter(Filter):
-    created_at__gte: Optional[datetime] = None
-    created_at__lte: Optional[datetime] = None
+    created_at__gte: Optional[datetime] = Field(None, alias="created_at_from")
+    created_at__lte: Optional[datetime] = Field(None, alias="created_at_to")
 
-    total__gte: Optional[Decimal] = None
-    total__lte: Optional[Decimal] = None
+    total__gte: Optional[Decimal] = Field(None, alias="total_from")
+    total__lte: Optional[Decimal] = Field(None, alias="total_to")
 
-    payment_type__in: Optional[list[ReceiptConstants.PaymentTypeEnum]] = None
+    payment_type__in: Optional[list[ReceiptConstants.PaymentTypeEnum]] = Field(
+        None, alias="payment_types"
+    )
 
     class Constants(Filter.Constants):
         model = Receipt
+
+    class Config:
+        populate_by_name = True
